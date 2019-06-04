83ffa6e213a48a0ea7328e7f9e37f753251de8ba
Galen Savidge <gsavidge@ucsc.edu>
2019-06-04 15:54:06 -0700

    Small edits to 42 instructions

--- Docs/html/_reference_frames_8h.js
+++ Docs/html/_reference_frames_8h.js
@@ -1,5 +1,6 @@
 var _reference_frames_8h =
 [
+    [ "DEG2RAD", "_reference_frames_8h.html#af7e8592d0a634bd3642e9fd508ea8022", null ],
     [ "EARTH_RADIUS_KM", "_reference_frames_8h.html#ac332db6b36a6bb914a098331e3ac37cf", null ],
     [ "PI", "_reference_frames_8h.html#a598a3330b3c21701223ee0ca14316eca", null ],
     [ "RAD2DEG", "_reference_frames_8h.html#ac5a945020d3528355cda82d383676736", null ],
